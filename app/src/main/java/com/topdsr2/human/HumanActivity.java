2f72aee97824e3336628acf492c18efc3146b98b
冬冬 <topdsr2@dongdongde-MacBook-Pro.local>
2019-01-15 08:37:12 +0800

    Initial commit

---
+++ app/src/main/java/com/topdsr2/human/HumanActivity.java
@@ -0,0 +1,13 @@
+package com.topdsr2.human;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HumanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_human);
+    }
+}
